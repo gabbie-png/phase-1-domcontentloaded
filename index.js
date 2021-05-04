db0089d8373f9bd3c6dc762799ba191cbc205c4e
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-05-03 21:46:06 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    const sugarBaby = document.getElementById('text')
+    sugarBaby.textContent = 'This is really cool!'
+})
